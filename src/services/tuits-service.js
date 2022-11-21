acb27b85e1f35926b354da23b1872ef4bd877546
sushmitha7598 <kurumaddali.s@northeastern.edu>
2022-11-20 22:38:48 -0500

    a8 REST fa22 changes

---
+++ src/services/tuits-service.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+//const TUITS_API = 'http://localhost:4000/api/tuits';
+//const TUITS_API = 'https://tuiter-node-server-app-sk.herokuapp.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
+
+export const createTuit = async (tuit) => {
+  const currentUser = {
+    "username": "NASA",
+    "handle": "@nasa",
+    "image": "nasa.png",
+   };
+   
+   const templateTuit = {
+    ...currentUser,
+    "topic": "Space",
+    "time": "2h",
+    "liked": false,
+    "replies": 0,
+    "retuits": 0,
+    "likes": 0,
+    "dislikes": 0,
+    "disliked": true
+   }
+
+    const newtuit = {...templateTuit, ...tuit}
+    const response = await axios.post(TUITS_API, newtuit)
+    return response.data;
+}
+export const findTuits  = async ()     => {
+    const response = await axios.get(TUITS_API);
+    const tuits = response.data;
+    return tuits;
+
+}
+export const deleteTuit = async (tid) => {
+    const response = await axios
+    .delete(`${TUITS_API}/${tid}`)
+  return response.data
+}
+export const updateTuit = async (tuit) => {
+    const response = await axios
+    .put(`${TUITS_API}/${tuit._id}`, tuit);
+  return tuit;
+}
--- src/tuiter/tuits/TuitsList/index.js
+++ src/tuiter/tuits/TuitsList/index.js
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 //import PostSummaryItem from "./PostSummary";
 import TuitsItem from "../TuitsItem";
-import {findTuitsThunk, deleteTuitThunk}
+import {findTuitsThunk}
   from "../../../services/tuits-thunks";
 
 const PostSummaryList = () => {
